c69cdb017f37528b7f1fc19ff44a8b2273131d2e
James Hawk <jph94880@gmail.com>
2023-09-26 17:11:56 -0500

    Adds new line at end of ItemForm.js

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -52,56 +52,5 @@ function ItemForm({ onItemFormSubmit }) {
   );
 }
 
-// here's how the abstracted version of the form would look:
-// function AbstractedItemForm({ onItemFormSubmit }) {
-//   const [formData, setFormData] = useState({
-//     name: "",
-//     category: "Produce",
-//   });
-
-//   function handleChange(event) {
-//     setFormData({
-//       ...formData,
-//       [event.target.name]: event.target.value,
-//     });
-//   }
-
-//   function handleSubmit(event) {
-//     event.preventDefault();
-//     onItemFormSubmit({
-//       id: uuid(),
-//       ...formData
-//     });
-//   }
-
-//   return (
-//     <form className="NewItem" onSubmit={handleSubmit}>
-//       <label>
-//         Name:
-//         <input
-//           type="text"
-//           name="name"
-//           value={formData.name}
-//           onChange={handleChange}
-//         />
-//       </label>
-
-//       <label>
-//         Category:
-//         <select
-//           name="category"
-//           value={formData.category}
-//           onChange={handleChange}
-//         >
-//           <option value="Produce">Produce</option>
-//           <option value="Dairy">Dairy</option>
-//           <option value="Dessert">Dessert</option>
-//         </select>
-//       </label>
-
-//       <button type="submit">Add to List</button>
-//     </form>
-//   );
-// }
 
 export default ItemForm;
